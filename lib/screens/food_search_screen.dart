5e4309c963f7c4e92e949a7ec195fdd79c7e7429
lebaaaa <yeetumas123@gmail.com>
2026-08-10 23:43:35 +0800

    added ai declaration

--- lib/screens/food_search_screen.dart
+++ lib/screens/food_search_screen.dart
@@ -51,7 +51,7 @@ class _FoodSearchScreenState extends State<FoodSearchScreen> {
                         borderRadius: BorderRadius.circular(10),
                       ),
                     ),
-                    onSubmitted: (_) => runSearch(),
+                    onSubmitted: (_) => runSearch(), //'_' suggested by AI as data returned by runSearch() is not used
                   ),
                 ),
                 const SizedBox(width: 8),
